cd0523ccaf0f0d08999e909cb26553ef57aab3b0
Donovan Baarda <abo@minkirri.apana.org.au>
2021-06-10 14:38:17 +1000

    Restore color logging to stderr.
    Add `log_prefix_t` function type and `log_prefix` function pointer for
    generating the logging prefix with or without colors. Add `log_prefix_plain()`
    and `log_prefix_color()` log_prefix_t implementations for plan and color
    prefixes. Add `_prefix()` macro for getting the prefix using `log_prefix()`,
    and make `_log()` macro use it. Update all the `lerr()`, `lwarn()`, etc macros
    to take into account _log() adds the alert level info using _prefix().
    
    Fix `ldebug()` to pass the LOG_DEBUG level to `_log()`.
    
    Update `log_init()` to set `log_prefix` with the right `log_prefix_plain()` or
    `log_prefix_color()` implementation.

--- log.c
+++ log.c
@@ -4,10 +4,39 @@
  * Licensed under the GPLv3 License. See LICENSE file for details.
  */
 #include "log.h"
+#include <err.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 log_func_t *log_func = errlog;
+log_prefix_t *log_prefix = log_prefix_color;
 int log_level = LOG_DEBUG;
+char prefix[256];               /* log_prefix result buffer. */
+
+const char *levelname[8] = {
+    "EMERGENCY!",               /* LOG_EMERG */
+    "ALERT",                    /* LOG_ALERT */
+    "CRITICAL",                 /* LOG_CRIT */
+    "error",                    /* LOG_ERR */
+    "warning",                  /* LOG_WARNING */
+    "note",                     /* LOG_NOTICE */
+    "info",                     /* LOG_INFO */
+    "debug"                     /* LOG_DEBUG */
+};
+
+const char *levelcolor[8] = {
+    "\33[01;91m",               /* LOG_EMERG - bold bright red */
+    "\33[01;91m",               /* LOG_ALERT - bold bright red */
+    "\33[01;91m",               /* LOG_CRIT - bold bright red */
+    "\33[01;31m",               /* LOG_ERR - bold red */
+    "\33[01;35m",               /* LOG_WARNING - bold magenta */
+    "\33[01;33m",               /* LOG_NOTICE - bold yellow */
+    "\33[01;36m",               /* LOG_INFO - bold cyan */
+    "\33[01;32m",               /* LOG_DEBUG - bold green */
+};
+
+#define LOCUS_C "\33[01m"       /* location info - bold */
+#define RESET_C "\33[0m"        /* reset colors */
 
 void log_init(const char *name, bool daemon, int level)
 {
@@ -15,8 +44,10 @@ void log_init(const char *name, bool daemon, int level)
         openlog(name, LOG_PID | LOG_NDELAY, LOG_DAEMON);
         setlogmask(LOG_UPTO(level));
         log_func = syslog;
+        log_prefix = log_prefix_plain;
     } else {
         log_func = errlog;
+        log_prefix = log_prefix_color;
     }
     log_level = level;
 }
@@ -29,4 +60,17 @@ void errlog(int level, const char *msg, ...)
     if (level <= log_level)
         vwarnx(msg, args);
     va_end(args);
+}
+
+char *log_prefix_plain(int level, const char *file, const unsigned line, const char *func)
+{
+    snprintf(prefix, sizeof(prefix), "%s:%u %s: %s: ", file, line, func, levelname[level]);
+    return prefix;
+}
+
+char *log_prefix_color(int level, const char *file, const unsigned line, const char *func)
+{
+    snprintf(prefix, sizeof(prefix), LOCUS_C "%s:%u %s: %s%s: " RESET_C, file, line, func, levelcolor[level],
+             levelname[level]);
+    return prefix;
 }
--- log.h
+++ log.h
@@ -7,21 +7,21 @@
 #ifndef LIGHTLDAPD_LOG_H
 #define LIGHTLDAPD_LOG_H
 
-#include <err.h>
 #include <errno.h>
 #include <string.h>
 #include <syslog.h>
 #include <stdbool.h>
 
-/** Function type for logging. */
+/** Logging function type. */
 typedef void log_func_t(int level, const char *msg, ...)
     __attribute__((__format__(printf, 2, 3)));
 
-/** Function to use for logging. */
-extern log_func_t *log_func;
+/** Logging prefix function type. */
+typedef char *log_prefix_t(int level, const char *file, const unsigned line, const char *func);
 
-/** The level to log upto. */
-extern int log_level;
+extern log_func_t *log_func;    /**< Logging function to use. */
+extern log_prefix_t *log_prefix;        /**< Logging prefix function to use. */
+extern int log_level;           /**< The level to log upto. */
 
 /** Initialize logging.
  *
@@ -35,21 +35,28 @@ extern int log_level;
  * \param level - The level to log upto. */
 void log_init(const char *name, bool daemon, int level);
 
-/** Logging to stderr with colors using warnx(). */
+/** Logging function to stderr using warnx(). */
 void errlog(int level, const char *msg, ...)
     __attribute__((__format__(printf, 2, 3)));
 
-#define _log(l, f, ...) log_func(l, "%s:%u %s: "f, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
+/** Logging prefix function without color. */
+char *log_prefix_plain(int level, const char *file, const unsigned line, const char *func);
 
-#define lerr(e, f, ...) do { _log(LOG_ERR, "Error: "f": %s", ##__VA_ARGS__, strerror(errno)); exit(e); } while (0)
-#define lerrx(e, f, ...) do { _log(LOG_ERR, "Error: "f, ##__VA_ARGS__); exit(e); } while (0)
-#define lwarn(f, ...) _log(LOG_WARNING, "Warning: "f": %s", ##__VA_ARGS__, strerror(errno))
-#define lwarnx(f, ...) _log(LOG_WARNING, "Warning: "f, ##__VA_ARGS__)
-#define lnote(f, ...) _log(LOG_NOTICE, "Note: "f, ##__VA_ARGS__)
-#define linfo(f, ...) _log(LOG_INFO, "Info: "f, ##__VA_ARGS__)
+/** Logging prefix function with color. */
+char *log_prefix_color(int level, const char *file, const unsigned line, const char *func);
+
+#define _prefix(l) log_prefix(l, __FILE__, __LINE__, __FUNCTION__)
+#define _log(l, f, ...) log_func(l, "%s"f, _prefix(l), ##__VA_ARGS__)
+
+#define lerr(e, f, ...) do { _log(LOG_ERR, f": %s", ##__VA_ARGS__, strerror(errno)); exit(e); } while (0)
+#define lerrx(e, f, ...) do { _log(LOG_ERR, f, ##__VA_ARGS__); exit(e); } while (0)
+#define lwarn(f, ...) _log(LOG_WARNING, f": %s", ##__VA_ARGS__, strerror(errno))
+#define lwarnx(f, ...) _log(LOG_WARNING, f, ##__VA_ARGS__)
+#define lnote(f, ...) _log(LOG_NOTICE, f, ##__VA_ARGS__)
+#define linfo(f, ...) _log(LOG_INFO, f, ##__VA_ARGS__)
 
 #ifdef DEBUG
-#define ldebug(f, ...) _log("Debug: "f, ##__VA_ARGS__)
+#define ldebug(f, ...) _log(LOG_DEBUG, f, ##__VA_ARGS__)
 #else
 #define ldebug(f, ...)
 #endif
